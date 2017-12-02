1e53e649d89162e2180829bb1476b815dbb69f8d
Kurt Lewis <kurt.jlewis15@gmail.com>
2017-12-02 15:09:39 -0500

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+const App = require('actions-on-google').DialogflowApp;
+const functions = require('firebase-functions');
+
+// a. the action name from the request_quote Dialogflow intent
+const QUOTE_ACTION = 'request_quote';
+// b. the parameters that are parsed from the request_quote intent
+const RAPPER_ARGUMENT = 'music-artist';
+
+exports.rapQuotes = functions.https.onRequest((request, response) => {
+    const app = new App({request, response});
+    console.log('Request headers: ' + JSON.stringify(request.headers));
+    console.log('Request body: ' + JSON.stringify(request.body));
+
+// c. The function that generates the rap quote
+    function requestQuote (app) {
+        let artist = app.getArgument(RAPPER_ARGUMENT);
+        app.tell('Kanye said: "Sometimes people write novels and they just be so wordy and so self-absorbed. I am not a fan of books. I would never want a books autograph. I am a proud non-reader of books."');
+    }
+    // d. build an action map, which maps intent names to functions
+    let actionMap = new Map();
+    actionMap.set(QUOTE_ACTION, requestQuote);
+
+app.handleRequest(actionMap);
+});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
